a601e9afa94fdce6b3ae41642497370e7b9298fd
Diogo Trindade <diogotr7@gmail.com>
2023-02-19 23:54:13 +0000

    Added LGS version

---
+++ src/Lgs/PipeReader.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace Lgs;
+
+public sealed class PipeReader : IDisposable
+{
+    private readonly NamedPipeServerStream _pipe;
+    private readonly CancellationTokenSource _cancellationTokenSource;
+    private readonly Task _listenerTask;
+    private readonly byte[] _buffer;
+
+    public event EventHandler<ReadOnlyMemory<byte>> CommandReceived;
+    public event EventHandler<Exception> Exception;
+    public event EventHandler Disconnected;
+
+    public PipeReader(NamedPipeServerStream pipe)
+    {
+        _pipe = pipe;
+        _buffer = new byte[_pipe.InBufferSize];
+        _cancellationTokenSource = new();
+        _listenerTask = Task.Run(ReadLoop);
+    }
+
+    private void ReadLoop()
+    {
+        while (!_cancellationTokenSource.IsCancellationRequested && _pipe.IsConnected)
+        {
+            try
+            {
+                int position = 0;
+                do
+                {
+                    position += _pipe.Read(_buffer, position, _buffer.Length - position);
+                } while (!_pipe.IsMessageComplete);
+
+                if (position == 0)
+                {
+                    continue;
+                }
+
+                var dataLength = (int)BitConverter.ToUInt32(_buffer, 0);
+                var actualData = _buffer.AsMemory(4, dataLength - 4);
+
+                CommandReceived?.Invoke(this, actualData);
+            }
+            catch(Exception e)
+            {
+                Exception?.Invoke(this, e);
+            }
+        }
+        _pipe.Dispose();
+        Disconnected?.Invoke(this, EventArgs.Empty);
+    }
+
+    #region IDisposable
+    private bool disposedValue;
+    private void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                _cancellationTokenSource.Cancel();
+                _listenerTask.Dispose();
+                try { _listenerTask.Wait(); } catch {/*we tried*/}
+                _cancellationTokenSource.Dispose();
+            }
+
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+    #endregion
+}
---
+++ src/Lgs/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lgs;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //\\.\pipe\LGS_LED_SDK-00000001
+        var listener = new PipeListener("LGS_LED_SDK-00000001");
+        listener.ClientConnected += (sender, eventArgs) => Console.WriteLine("Client connected");
+        listener.ClientDisconnected += (sender, eventArgs) => Console.WriteLine("Client disconnected");
+        listener.CommandReceived += (sender, bytes) =>
+        {
+            var length = bytes.Length;
+            var id = BitConverter.ToInt32(bytes.Span.Slice(0, 4));
+            switch (id)
+            {
+                case LogitechCommandIds.INITIALIZE: Initialize(bytes.Span[4..]); break;
+                case LogitechCommandIds.SAVE_LIGHTING: Console.WriteLine("Saving lighting"); break;
+                case LogitechCommandIds.SET_LIGHTING: Console.WriteLine("Setting lighting"); break;
+                case LogitechCommandIds.SET_LIGHTING_BITMAP: Console.WriteLine("Setting lighting from bitmap"); break;
+                case LogitechCommandIds.SET_LIGHTING_FOR_KEY_HID: Console.WriteLine("Setting lighting for key HID"); break;
+                case LogitechCommandIds.SET_LIGHTING_FOR_KEY_NAME: Console.WriteLine("Setting lighting for key name"); break;
+                case LogitechCommandIds.SET_LIGHTING_FOR_KEY_QUARTZ: Console.WriteLine("Setting lighting for key quartz"); break;
+                case LogitechCommandIds.SET_LIGHTING_FOR_KEY_SCAN: Console.WriteLine("Setting lighting for key scan"); break;
+                case LogitechCommandIds.SET_LIGHTING_FOR_TARGET_ZONE: Console.WriteLine("Setting lighting for target zone"); break;
+                case LogitechCommandIds.SET_TARGET_DEVICE: Console.WriteLine("Setting target device"); break;
+                case LogitechCommandIds.RESTORE_LIGHTING: Console.WriteLine("Restoring lighting"); break;
+                case LogitechCommandIds.RESTORE_LIGHTING_FOR_KEY: Console.WriteLine("Restoring lighting for key HID"); break;
+                case LogitechCommandIds.EXCLUDE_FROM_BITMAP: Console.WriteLine("Excluding from bitmap"); break;
+                case LogitechCommandIds.SAVE_LIGHTING_FOR_KEY: Console.WriteLine("Saving lighting for key HID"); break;
+                default: break;
+            }
+        };
+        listener.Exception += (sender, exception) => Console.WriteLine(exception);
+        
+        Console.ReadLine();
+    }
+
+    private static void Initialize(ReadOnlySpan<byte> span)
+    {
+        var filenameBytes = span.Slice(0, 512 * 2);
+        var filename = GetUnicodeTerminatedString(filenameBytes);
+        
+        Console.WriteLine($"Initializing with {filename}");
+    }
+
+    private static object GetUnicodeTerminatedString(ReadOnlySpan<byte> filenameBytes)
+    {
+        Span<byte> nullterm = stackalloc byte[2]
+        {
+            0, 0
+        };
+        var nulltermIndex = filenameBytes.IndexOf(nullterm);
+        return Encoding.Unicode.GetString(filenameBytes.Slice(0, nulltermIndex + 1));
+    }
+}
+
+public static class LogitechCommandIds
+{
+    public const int INITIALIZE = 2049;
+    public const int SET_LIGHTING = 2050;
+    public const int SET_TARGET_DEVICE = 2051;
+    public const int SAVE_LIGHTING = 2052;
+    public const int RESTORE_LIGHTING = 2053;
+    public const int SET_LIGHTING_FOR_KEY_SCAN= 2054;
+    public const int SET_LIGHTING_FOR_KEY_HID = 2055;
+    public const int SET_LIGHTING_FOR_KEY_QUARTZ = 2056;
+    public const int SET_LIGHTING_FOR_KEY_NAME = 2057;
+    public const int SET_LIGHTING_BITMAP = 2064;
+    public const int SAVE_LIGHTING_FOR_KEY = 2065;
+    public const int RESTORE_LIGHTING_FOR_KEY = 2066;
+    public const int EXCLUDE_FROM_BITMAP = 2067;
+    public const int SET_LIGHTING_FOR_TARGET_ZONE = 2080;
+}
